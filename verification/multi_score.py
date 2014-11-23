87279c3a406332f4beb71c9bf3790f43895d17e6
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-11-23 17:42:54 +0300

    Add referee

---
+++ verification/multi_score.py
@@ -0,0 +1,56 @@
+from checkio.referees.multicall import CheckiORefereeMulti
+from checkio import api
+from checkio.signals import PROCESS_ENDED
+
+REQ = 'req'
+REFEREE = 'referee'
+
+class CheckiORefereeMultiScore(CheckiORefereeMulti):
+    def __init__(self, *args, **kwargs):
+        self.total_score = 0
+        super().__init__(*args, **kwargs)
+
+    def on_ready(self, data):
+        self.code = data['code']
+        self.runner = data['runner']
+        if self.inspector:
+            result, result_message = self.inspector(self.code, self.runner)
+            if not result:
+                api.fail(0, result_message)
+        self.referee_data["seed"] = data.get("seed", "checkio")
+        self.start_env()
+
+        api.add_process_listener(REQ, PROCESS_ENDED, self.process_req_ended)
+
+    def check_current_test(self, data):
+        user_result = data['result']
+
+        self.referee_data = self.process_referee(self.referee_data, user_result)
+
+        self.total_score += self.referee_data.get("score", 0)
+
+        referee_result = self.referee_data.get("result", False)
+
+        if referee_result and self.is_win_referee:
+            is_win_result = self.is_win_referee(self.referee_data)
+        else:
+            is_win_result = referee_result.get("is_win", False)
+
+        self.referee_data.update({"is_win": is_win_result})
+
+        api.request_write_ext(self.referee_data)
+
+        if not referee_result:
+            if self.total_score:
+                api.success(score=self.total_score)
+            else:
+                return api.fail(self.current_step, self.get_current_test_fullname())
+
+        if not is_win_result:
+            self.test_current_step()
+        else:
+            self.total_score += self.referee_data["total_score"]
+            if self.next_env():
+                self.restart_env()
+            else:
+                api.success(score=self.total_score)
--- verification/referee.py
+++ verification/referee.py
@@ -25,23 +25,161 @@ checkio.referee.cover_codes
     cover_codes.unwrap_kwargs -- the same as unwrap_kwargs, but unwrap dict.
 
 """
+import random
 
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
 from checkio.referees import cover_codes
 from checkio.referees import checkers
+from multi_score import CheckiORefereeMultiScore
 
 from tests import TESTS
 
+
+SHIP = "S"
+TORNADO = "O"
+ROCK = "X"
+EMPTY = "."
+
+INIT_FUEL = 100
+
+HUNT_DISTANCE = 2
+
+DIRS = {
+    "N": (-1, 0),
+    "S": (1, 0),
+    "W": (0, -1),
+    "E": (0, 1),
+    ".": (0, 0)
+}
+
+
+def prepare_map(grid, ship, tornadoes):
+    grid = list(list(row) for row in grid)
+    grid[ship[0]][ship[1]] = SHIP
+    for t in tornadoes:
+        grid[t[0]][t[1]] = TORNADO
+    return tuple("".join(row) for row in grid)
+
+
+def init(init_data):
+    referee_data = {
+        "ship": (0, 0),
+        "old_tornadoes": init_data["tornadoes"],
+        "new_tornadoes": init_data["tornadoes"],
+        "tornado_moves": ["."] * len(init_data["tornadoes"]),
+        "map": init_data["map"],
+        "fuel": INIT_FUEL,
+        "input": prepare_map(init_data["map"], (1, 1), init_data["tornadoes"]),
+        "seed": init_data["seed"] + "".join(init_data["map"])
+    }
+    return referee_data
+
+def process(data, user_result):
+    if not isinstance(user_result, str) and user_result not in DIRS.keys():
+        data.update({
+            "result": False,
+            "result_addon": "You should return a string with an action."
+        })
+    sx, sy = data["ship"]
+    sea = data["map"]
+    sea_width = len(sea[0])
+    sea_height = len(sea)
+    tornadoes = data["new_tornadoes"]
+    new_sx, new_sy = sx + DIRS[user_result][0], sy + DIRS[user_result][1]
+    data["new_ship"] = new_sx, new_sy
+    if (new_sx, new_sy) in tornadoes:
+        data.update({
+            "result": False,
+            "result_addon": "Dont move in a tornado! SOS"
+        })
+        return data
+    if sea[new_sx][new_sy] == ROCK:
+        data.update({
+            "result": False,
+            "result_addon": "ROCK! Turn to ... SOS"
+        })
+        return data
+    if new_sx < 0 or new_sx >= len(sea) or new_sy < 0 or new_sy >= len(sea[0]):
+        data.update({
+            "result": False,
+            "result_addon": "Captain, we lost."
+        })
+        return data
+
+    data["fuel"] -= 1
+    if (new_sx, new_sy) == (sea_height - 1, sea_width - 1):
+        data.update({
+            "result": True,
+            "is_win": True,
+            "result_addon": "Captain, we lost."
+        })
+        return data
+
+    if data["fuel"] <= 0:
+        data.update({
+            "result": False,
+            "result_addon": "We don't have fuel."
+        })
+        return data
+    # Aaaaand tornado move
+    data["old_tornadoes"] = tornadoes[:]
+    tornado_moves = []
+    rseed = data["seed"] + user_result
+    random.seed(rseed)
+    for i in range(len(tornadoes)):
+        tx, ty = tornadoes[i]
+
+        possible = []
+        for direction, (dx, dy) in DIRS.items():
+            x, y = tx + dx, ty + dy
+            if 0 <= x < sea_height and 0 <= y < sea_width and sea[x][y] != ROCK and (x, y) not in tornadoes:
+                possible.append((direction, (x, y)))
+        distance = abs(sx - tx) + abs(sy - ty)
+        if distance <= HUNT_DISTANCE:
+            best = float("inf"), (tx, ty), "."
+            for d, (x, y) in possible:
+                possible_distance = abs(sx - x) + abs(sy - y)
+                if possible_distance < best[0]:
+                    best = possible_distance, (x, y), d
+                elif possible_distance == best[0]:
+                    best = random.choice([(possible_distance, (x, y), d), best])
+            tornadoes[i] = best[1]
+            tornado_moves.append(best[2])
+        else:
+            rand = random.choice(possible)
+            tornadoes[i] = rand[1]
+            tornado_moves.append(rand[0])
+    data.update({"new_tornadoes": tornadoes[:], "tornado_moves": tornado_moves})
+    if (new_sx, new_sy) in tornadoes:
+        data.update({
+            "result": False,
+            "result_addon": "Tornado caught us, Cap."
+        })
+        return data
+    data.update({
+        "result": True,
+        "result_addon": "Next move."
+    })
+    return data
+
+
+
+
+
+
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(
+    CheckiORefereeMultiScore(
         tests=TESTS,
         cover_code={
             'python-27': cover_codes.unwrap_args,  # or None
             'python-3': cover_codes.unwrap_args
         },
+        initial_referee=init,
+        process_referee=process,
+        is_win_referee=None
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
